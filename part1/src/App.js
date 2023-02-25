faa91818ea680e0e11415fcdbcb97585ba2e8f4b
Paul Kirsch <paul.kirsch96@gmail.com>
2023-02-25 12:54:27 +0100

    paul

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Hello = () => {
+  return(
+    <div>
+      <p>Hello World</p>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return (
+    <div>
+      <p>Greetings</p>
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+export default App
