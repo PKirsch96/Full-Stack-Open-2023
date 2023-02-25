398857dd185e84991a48582a6dd80ec5721e61ef
Paul Kirsch <paul.kirsch96@gmail.com>
2023-02-25 21:16:41 +0100

    polo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,53 +1,65 @@
-const Header = (props) =>{
-  return(
-    <h1>{props.course}</h1>
-  )
-}
 
-const Part = (props) =>{
-  return(
-    <>
-    <p>
-      {props.part} {props.exercises}
-    </p>
-    </>
-  )
-}
+  // Fonction pour calculer le total des exercices
+  const totalExercises = (props) => {
+    return props.reduce((sum, part) => sum + part.exercises, 0);
+  };
 
-const Content = (props) =>{
-  return(
-    <div>
-      <Part part = {props.part1} exercises = {props.exercises1} />
-      <Part part = {props.part2} exercises = {props.exercises2} />
-      <Part part = {props.part3} exercises = {props.exercises3} />
-    </div>
-  )
-}
+  // Fonction pour afficher l'en-tête
+  const Header = (props) => {
+    return <h1>{props.course.name}</h1>;
+  };
 
-const Total = (props) =>{
-  return(
-  <div>
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3} </p>
-  </div>
-  )
-}
+  // Fonction pour afficher les parties du cours
+  const Content = (props) => {
+    return (
+      <div>
+        {props.parts.map(part => (
+          <p key={part.name}>
+            {part.name} {part.exercises}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
+  // Fonction pour afficher le total des exercices
+  const Total = (props) => {
+    let somme = 0;
+    props.parts.forEach((part) => {
+      somme += part.exercises;
+    });
+    return "Number of exercises : " + somme;
+  };
+
 
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  // définitions des constantes
+  const course ={ 
+    name: "Half Stack application development",
+    parts: [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ]
+};
 
-  return(
+  // Affichage global de l'application
+  return (
     <div>
-      <Header course = {course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />    
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
 export default App
