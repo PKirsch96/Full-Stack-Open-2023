ac6e775c88eef11401f55b7729f7c8e6a67e6662
Paul Kirsch <paul.kirsch96@gmail.com>
2023-02-26 14:24:07 +0100

    paul

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,39 +1,63 @@
+import { useState } from "react"
+//Fonction qui retourne une phrase de bienvenue et devinne l'année de naissance
+const Hello = ({name, age}) =>{
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - age
+  }
+  return(
+    <div>
+      <p>
+        Hello {name}. You are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()} </p>
+    </div>
+  )
+}
 
-  // Fonction pour calculer le total des exercices
-  const totalExercises = (props) => {
-    return props.reduce((sum, part) => sum + part.exercises, 0);
-  };
+//affiche l'etat du conteur de click
+const Display = ({counter}) => <div> {counter} </div>
 
-  // Fonction pour afficher l'en-tête
-  const Header = (props) => {
-    return <h1>{props.course.name}</h1>;
-  };
+//parametre la fonction des boutans
+const Button = ({ onClick, text}) => <button onClick = {onClick}>{text}</button> 
+
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
 
   // Fonction pour afficher les parties du cours
-  const Content = (props) => {
-    return (
-      <div>
-        {props.parts.map(part => (
-          <p key={part.name}>
-            {part.name} {part.exercises}
-          </p>
-        ))}
-      </div>
-    );
-  };
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+   );
+};
 
   // Fonction pour afficher le total des exercices
-  const Total = (props) => {
-    let somme = 0;
-    props.parts.forEach((part) => {
-      somme += part.exercises;
-    });
-    return "Number of exercises : " + somme;
-  };
+const Total = (props) => {
+  let somme = 0;
+  props.parts.forEach((part) => {
+    somme += part.exercises;
+  });
+  return "Number of exercises : " + somme;
+};
 
 
 const App = () => {
   // définitions des constantes
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setZero = () => setCounter(0)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+
   const course ={ 
     name: "Half Stack application development",
     parts: [
@@ -52,13 +76,35 @@ const App = () => {
   ]
 };
 
+console.log("rendering ...", counter)
+
   // Affichage global de l'application
   return (
+    <>
     <div>
+      <Hello name="paul" age = '27' />
       <Header course={course} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
+    <div>
+      <Display counter = {counter} />
+    </div>
+    <div>
+      <Button
+        onClick = {increaseByOne}
+        text = "plus"
+      />
+      <Button
+        onClick = {setZero}
+        text = "zero"
+      />
+      <Button
+        onClick = {decreaseByOne}
+        text = "minus"
+      />
+    </div>
+    </>
   );
 };
 
